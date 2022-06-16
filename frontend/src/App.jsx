ff5cead12d867107e2973181b25be1cc96a8f8ba
Fernando Segregio <segregio@gmail.com>
2022-06-16 01:14:16 -0300

    feat: define route Login and /

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Login from './components/login';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
     <>
-      Grao
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        {' '}
+        {/* <Route path="/table" element={<Search />} /> */}
+      </Routes>
+      <GlobalStyle />
     </>
   );
 }
