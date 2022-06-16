cfc88666dee379b2e67255b95bdce84ba1d2c6a2
Fernando Segregio <segregio@gmail.com>
2022-06-16 01:10:41 -0300

    feat: create component Login

---
+++ frontend/src/components/login/index.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import LoginContainer from './style';
+
+function Login() {
+  const [login, setLogin] = useState({ email: '', password: '' });
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setLogin((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const PASS_LENGTH = 6;
+    const { email, password } = login;
+    const REGEX_EMAIL = /^[\w.]+@[\w.]+\w+\.\w+$/;
+    const validateMail = REGEX_EMAIL.test(email);
+    const validatePassword = password.length >= PASS_LENGTH;
+    setIsDisabled(!(validateMail && validatePassword));
+  }, [login]);
+
+  return (
+    <LoginContainer>
+      <div>
+        <h3>Faça seu login, e </h3>
+        <h1>conheça os melhores Restaurantes</h1>
+      </div>
+      <form>
+        <input
+          type="text"
+          name="email"
+          placeholder="email@email.com"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="senha"
+          onChange={handleChange}
+        />
+        <button
+          onClick={() => { navigate('/register'); }}
+          type="button"
+          disabled={isDisabled}
+        >
+          Entrar
+
+        </button>
+      </form>
+    </LoginContainer>
+  );
+}
+
+export default Login;
