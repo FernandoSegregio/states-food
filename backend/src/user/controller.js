7f950ec4d233a9ce0398c782ca4ad0e736e0ba10
Fernando Segregio <segregio@gmail.com>
2022-06-16 23:21:44 -0300

    feat: create controller login

--- backend/src/user/controller.js
+++ backend/src/user/controller.js
@@ -2,10 +2,16 @@ import userService from './service.js';
 
 class User {
   async find(req, res) {
-    const { email } = req.body;
+    const { email } = req.tokenData;
     const { code, user, message } = await userService.find(email);
     if (message) res.status(code).json(message);
     return res.status(code).json(user);
+  }
+  async login(req, res) {
+    const { email, password } = req.body;
+    const { code, token, message } = await userService.login({email, password});
+    if (message) res.status(code).json(message);
+    return res.status(code).json(token);
   }
 }
 
