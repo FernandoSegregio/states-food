211ca4aea76f02e29fdaccd623a37e6bbd362da8
Fernando Segregio <segregio@gmail.com>
2022-06-16 23:20:57 -0300

    feat: create service login

--- backend/src/user/service.js
+++ backend/src/user/service.js
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client';
+import jwtGenarator from '../helpers/jwtGenarator.js';
+import md5 from 'md5';
 
 const prisma = new PrismaClient();
 
@@ -12,6 +14,23 @@ class User {
       return { code: 500, message: error.message };
     }
   }
+
+  async login({email, password}) {
+    try {
+      const secretPassword = md5(password);
+      const verifyUser = await this.find(email);
+      if (!verifyUser) return { code: 404, message: 'user not found' };
+      if (verifyUser.user.password !== secretPassword) {
+        return { code: 400, message: 'invalid password' };
+      }
+      const token = jwtGenarator({email});
+      return { code: 200, token }
+    } catch (error) {
+      return { code: 500, message: error.message };
+    }
+  }
+
+
 }
 
 export default new User();
