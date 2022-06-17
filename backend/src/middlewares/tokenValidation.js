4e487b3d2f5a1723549795897f69b74fe1e769e5
Fernando Segregio <segregio@gmail.com>
2022-06-16 23:23:00 -0300

    feat: create middleware tokenValidation

---
+++ backend/src/middlewares/tokenValidation.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+export default async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ message: 'Token not found' });
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.tokenData = decode.data;
+
+    next();
+  } catch (error) {
+    // console.log(error);
+    if (error.name.includes('Token')) {
+      return res.status(401).json({ message: 'Expired or invalid token' });
+    }
+    next(error);
+  }
+};
