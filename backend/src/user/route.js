8415aa8f289333002b8bdc7feaf027b47b915b65
Fernando Segregio <segregio@gmail.com>
2022-06-16 23:23:51 -0300

    feat: crete route user get and post

--- backend/src/user/route.js
+++ backend/src/user/route.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import tokenValidation from '../middlewares/tokenValidation.js';
 
 import controller from './controller.js';
 
 const route = Router();
 
-route.get('/', controller.find);
+route.get('/', tokenValidation, controller.find);
+route.post('/', controller.login);
 
 export default route;
