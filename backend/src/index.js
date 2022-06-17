76ff1faadb537f7a34a58d4cf3f941d5f7600ae4
Fernando Segregio <segregio@gmail.com>
2022-06-16 23:38:30 -0300

    feat: add app use, images public and /restaurant

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import loginRouter from './user/route.js';
+import restaurantRouter from './restaurant/route.js';
 
 const app = express();
+
+app.use('/images', express.static('public'));
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/login', loginRouter);
+app.use('/restaurant', restaurantRouter);
 
 const PORT = 3001;
 
