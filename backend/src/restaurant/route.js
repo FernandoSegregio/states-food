9225e35b4689e243d7b6354416cff87b9ac95258
FernandoSegregio <segregio@gmail.com>
2022-06-19 20:26:06 -0300

    feat: add token validation in route restaurant

--- backend/src/restaurant/route.js
+++ backend/src/restaurant/route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import tokenValidation from '../middlewares/tokenValidation.js'
 
 import controller from './controller.js';
 
 const router = Router();
 
-router.get('/', controller.find);
+router.get('/', tokenValidation, controller.find);
 
 export default router;
