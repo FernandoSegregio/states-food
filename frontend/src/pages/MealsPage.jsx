703b2b17bf9fd5ad58af790a060f15cf9c5aa5d1
FernandoSegregio <segregio@gmail.com>
2022-06-20 15:09:37 -0300

    feat: render component Header in pages

--- frontend/src/pages/MealsPage.jsx
+++ frontend/src/pages/MealsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MenuMeals from '../components/meals';
 import Footer from '../components/footer';
+import Header from '../components/header';
 
 function MealsPage() {
   return (
     <>
+      <Header />
       <MenuMeals />
       <Footer />
     </>
--- frontend/src/pages/RestaurantsPage.jsx
+++ frontend/src/pages/RestaurantsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Restaurants from '../components/restaurant';
 import Footer from '../components/footer';
+import Header from '../components/header';
 
 function RestaurantsPage() {
   return (
     <>
+      <Header />
       <Restaurants />
       <Footer />
     </>
