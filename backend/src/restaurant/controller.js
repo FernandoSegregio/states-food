33a7aa0e40bc7faeb4af088e3e93f2caf466f80c
FernandoSegregio <segregio@gmail.com>
2022-06-19 22:54:31 -0300

    feat: create metod findMeals in controller restaurant

--- backend/src/restaurant/controller.js
+++ backend/src/restaurant/controller.js
@@ -6,6 +6,13 @@ class Restaurant {
     if (message) res.status(code).json(message);
     return res.status(code).json(restaurants);
   }
+  async findMeals(req, res) {
+    const { id } = req.params;
+    console.log(id);
+    const { code, meals, message } = await restaurantService.findMeals(id);
+    if (message) res.status(code).json(message);
+    return res.status(code).json(meals);
+  }
 }
 
 export default new Restaurant();
