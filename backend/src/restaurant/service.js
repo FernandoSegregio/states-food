0e397e71a806f1b7325790a83ce7014995cb081e
FernandoSegregio <segregio@gmail.com>
2022-06-20 04:00:06 -0300

    feat: add service menuDrink

--- backend/src/restaurant/service.js
+++ backend/src/restaurant/service.js
@@ -21,6 +21,15 @@ class Restaurant {
       return { code: 500, message: error.message };
     }
   }
+  async findDrink(id) {
+    try {
+      const drink = await prisma.menuDrink.findMany({ where: { restaurantId: Number(id) }});
+      if (!drink) return { code: 404, message: 'drink not found' };
+      return { code: 200, drink };
+    } catch (error) {
+      return { code: 500, message: error.message };
+    }
+  }
 }
 
 export default new Restaurant();
