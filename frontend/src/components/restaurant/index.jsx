cdeb30b26c7fa28311b7a6ce11cf263546c63490
FernandoSegregio <segregio@gmail.com>
2022-06-20 18:08:40 -0300

    feat: add logical search bar

--- frontend/src/components/restaurant/index.jsx
+++ frontend/src/components/restaurant/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { nanoid } from 'nanoid';
 import { useNavigate } from 'react-router-dom';
 import { getItems } from '../../services/axiosRequest';
@@ -11,9 +11,13 @@ import {
   RestaurantImg,
   TextContainer,
 } from './style';
+import StatesFoodsContext from '../../context/StatesFoodsContext';
 
 function Restaurants() {
   const [restaurant, setRestaurant] = useState([]);
+  const [foods, setFoods] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+  const { filtered } = useContext(StatesFoodsContext);
 
   const navigate = useNavigate();
 
@@ -21,42 +25,60 @@ function Restaurants() {
     const token = JSON.parse(localStorage.getItem('token'));
     const item = await getItems('/restaurant', token);
     setRestaurant([...item]);
+
+    const food = await getItems('/restaurant/food', token);
+    setFoods([...food]);
+    const drink = await getItems('/restaurant/drink', token);
+    setDrinks([...drink]);
   }
 
   useEffect(() => {
     requestItems();
+    console.log(foods);
   }, []);
+
+  const searchItems = Array.of(...restaurant, ...foods, ...drinks);
+
+  const filter = searchItems.filter((item) => item.name.toLowerCase().includes(filtered)
+  || item.description.toLowerCase().includes(filtered));
+
+  const schemaFilter = {
+    false: filter,
+    true: restaurant,
+  };
 
   return (
     <RestaurantContainer>
-      { restaurant && restaurant.map(({
-        url_image: imageUrl, description, flag, name, rate, time, delivery, id,
-      }) => (
-        <CardRestaurant key={nanoid()} onClick={() => navigate(`/restaurants/${id}`)}>
-          <RestaurantImg src={imageUrl} alt={name} />
-          <div>
-            <FlagImg src={flag} alt="" />
-            <TextContainer>
-              <NameRestaurant>{name}</NameRestaurant>
-              <DescriptionRestaurant>{description}</DescriptionRestaurant>
-              <div>
+      { restaurant && schemaFilter[filtered.length === 0].map(
+        ({
+          url_image: imageUrl, description, flag, name, rate, time, delivery, id,
+        }) => (
+          <CardRestaurant key={nanoid()} onClick={() => navigate(`/restaurants/${id}`)}>
+            <RestaurantImg src={imageUrl} alt={name} />
+            <div>
+              { flag && <FlagImg src={flag} alt="" />}
+              <TextContainer>
+                <NameRestaurant>{name}</NameRestaurant>
+                <DescriptionRestaurant>{description}</DescriptionRestaurant>
                 <div>
-                  <span className="iconify" data-icon="ep:star-filled" />
-                  <span>{rate}</span>
+                  <div>
+                    { rate && <span className="iconify" data-icon="ep:star-filled" />}
+                    <span>{rate}</span>
+                  </div>
+                  <div>
+                    { time && <span className="iconify" data-icon="ic:outline-watch-later" />}
+                    <span>{time}</span>
+                  </div>
+                  <div>
+                    { delivery && <span className="iconify delivery" data-icon="iconoir:delivery-truck" />}
+                    <span>{delivery}</span>
+                  </div>
                 </div>
-                <div>
-                  <span className="iconify" data-icon="ic:outline-watch-later" />
-                  <span>{time}</span>
-                </div>
-                <div>
-                  <span className="iconify delivery" data-icon="iconoir:delivery-truck" />
-                  <span>{delivery}</span>
-                </div>
-              </div>
-            </TextContainer>
-          </div>
-        </CardRestaurant>
-      ))}
+              </TextContainer>
+            </div>
+          </CardRestaurant>
+        ),
+      )}
     </RestaurantContainer>
   );
 }
