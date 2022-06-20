c7da49a38dfb05dbcddec18b6699c75989d4d618
FernandoSegregio <segregio@gmail.com>
2022-06-20 04:03:45 -0300

    feat: creat MealsPage

---
+++ frontend/src/pages/MealsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuMeals from '../components/meals';
+import Footer from '../components/footer';
+
+function MealsPage() {
+  return (
+    <>
+      <MenuMeals />
+      <Footer />
+    </>
+  );
+}
+
+export default MealsPage;
