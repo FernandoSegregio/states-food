f64e49d2d1d529a83b44e9d972abd9f166fcff7b
FernandoSegregio <segregio@gmail.com>
2022-06-21 14:12:21 -0300

    feat: add setFiltered in searchBar

--- frontend/src/components/footer/index.jsx
+++ frontend/src/components/footer/index.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import StatesFoodsContext from '../../context/StatesFoodsContext';
 // import { useNavigate } from 'react-router-dom';
 import { FooterContainer, SearchBar } from './style';
 
 function Footer() {
   const [isDisable, setIsDisable] = useState(true);
   // const navigate = useNavigate();
+  const { setFiltered } = useContext(StatesFoodsContext);
 
   function openSearchBar() {
     return isDisable
@@ -26,7 +28,12 @@ function Footer() {
       <button type="button">
         <span className="iconify" data-icon="iconoir:profile-circled" />
       </button>
-      <SearchBar type="text" hidden={isDisable} placeholder="Pesquise por um Restaurante ou Prato" />
+      <SearchBar
+        type="text"
+        hidden={isDisable}
+        placeholder="Pesquise por um Restaurante ou Prato"
+        onChange={(e) => setFiltered(e.target.value)}
+      />
     </FooterContainer>
   );
 }
