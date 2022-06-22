f3deea1d3dae9455edac673087250827239cce32
FernandoSegregio <segregio@gmail.com>
2022-06-22 13:01:13 -0300

    fix: remove Header and Footer screen Login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import Login from '../components/login';
-import Footer from '../components/footer';
-import Header from '../components/header';
 
 function LoginPage() {
   return (
-    <>
-      <Header />
-      <Login />
-      <Footer />
-    </>
+    <Login />
   );
 }
 
